7c92922d37f5f48acb83fa15425b1a0d2528e9c4
Nathan Typanski <ntypanski@gmail.com>
2013-12-15 20:19:51 -0500

    Removes redundant evil-mode keybindings

--- config/init-evil.el
+++ config/init-evil.el
@@ -84,13 +84,4 @@
 (defadvice evil-ex-search-previous (after advice-for-evil-ex-search-previous activate)
   (evil-scroll-line-to-center (line-number-at-pos)))
 
-;;; esc quits
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-
 (provide 'init-evil)
