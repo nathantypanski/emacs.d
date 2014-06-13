da1ac9af6baae0c2c187d55826d5c7664db31083
Nathan Typanski <ntypanski@gmail.com>
2014-06-13 10:30:17 -0400

    interactive haskell

--- config/my-haskell.el
+++ config/my-haskell.el
@@ -4,35 +4,27 @@
 
 (use-package haskell-mode
   :ensure haskell-mode
+  :commands haskell-mode
   :mode ("\\.hs\\'" . haskell-mode)
   :config
   (progn
-    (define-key haskell-mode-map (kbd "C-x C-d") nil)
-    (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-    (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
-    (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
-    (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-    (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-    (define-key haskell-mode-map (kbd "C-c M-.") nil)
-    (define-key haskell-mode-map (kbd "C-c C-d") nil)
-    (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile)
-    (add-hook 'haskell-mode-hook
-              (lambda ()
-                (turn-on-haskell-doc-mode)
-                (after 'evil
-                  (setq evil-auto-indent nil))
-                (turn-on-haskell-indentation)
-                (ghc-init)
-                (purty-mode)
-                )
-              )
-    (require 'haskell-mode-autoloads)
-    ;;(use-package inf-haskell)
-    ;;(use-package haskell-cabal
-    ;;  :init
-    ;;  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile)
-    ;;  )
-    )
+    (setq haskell-process-show-debug-tips nil)
+    (defun my-haskell-autoloads ()
+      "Autoloads for entering Haskell-mode."
+      (turn-on-haskell-doc-mode)
+      (after 'evil
+        (setq evil-auto-indent nil)
+        )
+      (turn-on-haskell-indentation))
+    (add-hook 'haskell-mode-hook 'my-haskell-autoloads)
+    (evil-define-key 'normal haskell-mode-map (kbd "C-x C-d") nil)
+    (evil-define-key 'normal haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+    (evil-define-key 'normal haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+    (evil-define-key 'normal haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+    (evil-define-key 'normal haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+    (evil-define-key 'normal haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+    (evil-define-key 'normal haskell-mode-map (kbd "C-c M-.") nil)
+    (evil-define-key 'normal haskell-mode-map (kbd "C-c C-d") nil))
   )
 
 (provide 'my-haskell)
