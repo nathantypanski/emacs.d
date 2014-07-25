919cb13f847c17ff1812af155af3a8cec7a3c416
Nathan Typanski <ntypanski@gmail.com>
2014-07-25 17:57:14 -0400

    better markdown-mode

--- config/my-markdown.el
+++ config/my-markdown.el
@@ -1,3 +1,11 @@
-(add-to-list 'auto-mode-alist '("\\.page\\'" . markdown-mode))
+(use-package markdown-mode
+  :commands markdown-mode
+  :ensure markdown-mode
+  :init (progn
+        (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+        (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+        (add-to-list 'auto-mode-alist '("\\.page\\'" . markdown-mode))
+        )
+  )
 
 (provide 'my-markdown)
