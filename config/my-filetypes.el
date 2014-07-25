2a424c02f0c94ed17345a7a12e06d7a5ee12c9c0
Nathan Typanski <ntypanski@gmail.com>
2014-07-25 19:39:55 -0400

    my-filetypes: remove verilog cruft

--- config/my-filetypes.el
+++ config/my-filetypes.el
@@ -1,12 +1,3 @@
-;; verilog
-;;(add-to-list 'auto-mode-alist '("\\.v\\'" . verilog-mode))
-;;(add-hook verilog-mode-hook (progn (setq verilog-indent-level 8)
-;;                                   (setq verilog-indent-level-module 8)
-;;                                   (setq verilog-indent-level-declaration 8)
-;;                                   (setq verilog-tab-always-indent t)
-;;                                   )
-;;          )
-
 ;; zsh shell script mode
 (require 'rust-mode)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
