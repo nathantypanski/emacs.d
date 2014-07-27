c23ff2a53c8405338575e40202a14aabfadafe60
Nathan Typanski <ntypanski@gmail.com>
2014-07-27 12:38:44 -0400

    init.el: remove marmalade

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
                          ("org" . "http://orgmode.org/elpa/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 ;;(byte-recompile-directory "~/.emacs.d")
 (require 'package)
