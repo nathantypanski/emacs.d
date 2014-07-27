9aa4b32d64109c7b3283c9d013dc8f40078e299e
Nathan Typanski <ntypanski@gmail.com>
2014-07-27 12:36:46 -0400

    C: better tab configuration

--- config/my-c.el
+++ config/my-c.el
@@ -1,5 +1,14 @@
-(setq-default c-default-style "linux"
-              c-basic-offset 8
-              tab-width 8)
+(setq c-default-style '((java-mode . "java")
+                        (awk-mode . "awk")
+                        (other . "linux")))
+(defun my-set-evil-shift-width ()
+  "Set Evil's shift width for C editing."
+  (after 'evil
+    (setq evil-shift-width 8)
+    )
+  )
+
+(add-hook 'c-initialization-hook 'my-set-evil-shift-width)
+
 (c-set-offset 'case-label '+)
 (provide 'my-c)
