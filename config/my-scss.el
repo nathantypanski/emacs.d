4733095e4f6472898589b6ab412f3f320ee70b9b
Nathan Typanski <ntypanski@gmail.com>
2014-07-27 12:38:03 -0400

    scss/css: proper startup bindings

--- config/my-scss.el
+++ config/my-scss.el
@@ -2,9 +2,16 @@
 ;;
 ;; for things related to scss
 
+(use-package css-mode
+  :commands css-mode
+  :ensure css-mode
+  :mode "\\.css\\'"
+  )
+
 (use-package scss-mode
+  :commands scss-mode
   :ensure scss-mode
-  :mode ("\\.scss\\'" . scss-mode)
-    )
+  :mode "\\.scss\\'"
+)
 
 (provide 'my-scss)
