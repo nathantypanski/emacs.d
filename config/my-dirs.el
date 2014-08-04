3eb6dab3cc8f7eaffd0ac228935fe8211668a1e0
Nathan Typanski <ntypanski@gmail.com>
2014-08-03 23:17:45 -0400

    custom dired bindings

--- config/my-dirs.el
+++ config/my-dirs.el
@@ -47,5 +47,21 @@
               )
     )
   )
+(setq dired-listing-switches "-aBhl  --group-directories-first")
+
+(after 'evil
+  (evil-define-key 'normal dired-mode-map "h" 'dired-up-directory)
+  (evil-define-key 'normal dired-mode-map "l" 'dired-find-alternate-file)
+  (evil-define-key 'normal dired-mode-map "a" 'ag-dired)
+  (evil-define-key 'normal dired-mode-map "o" 'dired-sort-toggle-or-edit)
+  (evil-define-key 'normal dired-mode-map "v" 'dired-toggle-marks)
+  (evil-define-key 'normal dired-mode-map "m" 'dired-mark)
+  (evil-define-key 'normal dired-mode-map "u" 'dired-undo)
+  (evil-define-key 'normal dired-mode-map "U" 'dired-unmark-all-marks)
+  (evil-define-key 'normal dired-mode-map "c" 'dired-create-directory)
+  (evil-define-key 'normal dired-mode-map "n" 'evil-search-next)
+  (evil-define-key 'normal dired-mode-map "N" 'evil-search-previous)
+  (evil-define-key 'normal dired-mode-map "q" 'kill-this-buffer)
+  )
 
 (provide 'my-dirs)
