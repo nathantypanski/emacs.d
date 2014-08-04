3648f56d9b25c32b6b2b1373d7e0c72a22433721
Nathan Typanski <ntypanski@gmail.com>
2014-08-03 23:13:30 -0400

    C: use semantic mode stuff

--- config/my-c.el
+++ config/my-c.el
@@ -11,4 +11,24 @@
 (add-hook 'c-initialization-hook 'my-set-evil-shift-width)
 
 (c-set-offset 'case-label '+)
+
+(use-package semantic
+  :ensure semantic
+  :init
+  (progn
+    (global-semanticdb-minor-mode 1)
+    )
+)
+(use-package cedet
+  :ensure cedet
+  :config
+  (progn
+    (after 'evil
+      (evil-define-key 'insert c-mode-map (kbd "TAB") 'c-indent-line-or-region)
+      )
+    (semantic-mode)
+))
+
+
+
 (provide 'my-c)
