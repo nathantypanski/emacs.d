9a941061bd8e042825432d2f7bbdf7265f32d5cd
Nathan Typanski <ntypanski@gmail.com>
2014-08-11 00:30:45 -0400

    rust-mode: electric rbrace

--- config/my-rust.el
+++ config/my-rust.el
@@ -5,7 +5,27 @@
   :init
   (add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
   :config
-    (add-hook 'rust-mode-hook (lambda ()
-    setq tab-width 4)))
+  (progn
+    (defun my-rust-electric-rbrace (arg)
+      "Insert a rbrace, and then indent the line properly"
+      (interactive "*P")
+      (insert "}")
+      (rust-mode-indent-line)
+      )
+
+    (defvar rust-mode-map () "Keymap used in Rust mode.")
+    (if rust-mode-map nil
+      (setq rust-mode-map (make-sparse-keymap))
+      (define-key rust-mode-map "}" 'my-rust-electric-rbrace)
+      (evil-define-key 'insert rust-mode-map "}" 'my-rust-electric-rbrace)
+    )
+    (defun my-rust-setup ()
+      "Make rust do things the way I like it."
+      (setq tab-width 4)
+      (use-local-map rust-mode-map)
+      )
+    (add-hook 'rust-mode-hook 'my-rust-setup)
+    )
+  )
 
 (provide 'my-rust)
