2c0ab4c3c9c38c2f78b36a2c96de0d521203ecbb
Nathan Typanski <ntypanski@gmail.com>
2014-08-10 23:24:54 -0400

    word wrap markdown mode by default

--- config/my-markdown.el
+++ config/my-markdown.el
@@ -5,6 +5,7 @@
         (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
         (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
         (add-to-list 'auto-mode-alist '("\\.page\\'" . markdown-mode))
+        (add-hook 'markdown-mode-hook 'visual-line-mode)
         )
   )
 
