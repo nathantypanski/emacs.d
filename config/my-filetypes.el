934030a41a33537108f3804e500007eb010736b7
Nathan Typanski <ntypanski@gmail.com>
2014-09-03 00:05:48 -0400

    remove rust from my-filetypes.el

--- config/my-filetypes.el
+++ config/my-filetypes.el
@@ -1,5 +1,3 @@
 ;; zsh shell script mode
-(require 'rust-mode)
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 (add-to-list 'auto-mode-alist '("/mutt" . mail-mode))
 (provide 'my-filetypes)
