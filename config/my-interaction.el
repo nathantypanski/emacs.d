2906fd8a7498440e0e551bc3ded98fd6b45353c1
Nathan Typanski <ntypanski@gmail.com>
2014-09-13 13:32:39 -0400

    separate ag setup into function instead of lambdas

--- config/my-interaction.el
+++ config/my-interaction.el
@@ -131,9 +131,13 @@
   (progn
     (setq ag-highlight-search t)
     (setq ag-reuse-buffers t)
-    (add-hook 'ag-mode-hook (lambda () (toggle-truncate-lines t)))
-    (add-hook 'ag-mode-hook (lambda () (linum-mode 0)))
-    (add-hook 'ag-mode-hook (lambda () (switch-to-buffer-other-window "*ag search*")))
+    (defun my-setup-ag ()
+      "Function called to set my ag stuff up."
+      (toggle-truncate-lines t)
+      (linum-mode 0)
+      (switch-to-buffer-other-window "*ag search*")
+      )
+    (add-hook 'ag-mode-hook 'my-setup-ag)
     )
   )
 
