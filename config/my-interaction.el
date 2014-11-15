d244e45872d370473f206639e1a10f9a2b186cfe
Nathan Typanski <ntypanski@gmail.com>
2014-11-15 12:44:54 -0500

    my-interaction: style + cleanup

--- config/my-interaction.el
+++ config/my-interaction.el
@@ -2,14 +2,17 @@
 ;;
 ;; smart emacs navigation, etc.
 
+
 (use-package ctags-update
   :ensure ctags-update
   :init
   (progn
     (add-hook 'c-mode-common-hook  'turn-on-ctags-auto-update-mode)
     (add-hook 'emacs-lisp-mode-hook  'turn-on-ctags-auto-update-mode)
-    (autoload 'turn-on-ctags-auto-update-mode "ctags-update" "turn on `ctags-auto-update-mode'." t)
-    ))
+    (autoload 'turn-on-ctags-auto-update-mode
+      "ctags-update"
+      "turn on `ctags-auto-update-mode'." t)))
+
 
 (use-package flycheck
   :ensure flycheck
@@ -26,23 +29,16 @@
       "Jump to flycheck errors and switch to the errorlist buffer"
       (interactive)
       (flycheck-list-errors)
-      (switch-to-buffer-other-window "*Flycheck errors*" t)
-      )
+      (switch-to-buffer-other-window "*Flycheck errors*" t))
 
     (after 'evil
-      (evil-define-key 'normal flycheck-error-list-mode-map "k" #'flycheck-error-list-previous-error)
-      (evil-define-key 'normal flycheck-error-list-mode-map "j" #'flycheck-error-list-next-error)
-      (evil-define-key 'normal flycheck-error-list-mode-map "K" #'evil-previous-line)
-      (evil-define-key 'normal flycheck-error-list-mode-map "J" #'evil-next-line)
-      (evil-define-key 'normal flycheck-error-list-mode-map (kbd "RET") #'flycheck-error-list-goto-error)
-      (evil-define-key 'normal flycheck-error-list-mode-map "q" 'quit-window)
-
-      )
-    )
-  )
-
-
-
+      (evil-define-key 'normal flycheck-error-list-mode-map
+        "q" 'quit-window
+        "j" #'flycheck-error-list-next-error
+        "k" #'flycheck-error-list-previous-error
+        "K" #'evil-previous-line
+        "J" #'evil-next-line
+        (kbd "RET") #'flycheck-error-list-goto-error))))
 
 
 (use-package ace-jump-mode
@@ -60,19 +56,7 @@
         (setq ace-jump-mode-move-keys
               (nconc '(?j ?f ?k ?d ?l ?s ?a ?h ?g)
                      '(?y ?t ?u ?r ?i ?e ?o ?w ?p ?q)
-                     '(?n ?v ?b ?m ?c ?x ?z)))
-        (define-key evil-normal-state-map (kbd "SPC j") 'ace-jump-char-mode)
-        (define-key evil-motion-state-map (kbd "SPC") 'evil-ace-jump-char-mode)
-        (define-key evil-motion-state-map (kbd "S-SPC") 'evil-ace-jump-line-mode)
-        )
-      ;; These will definitely work:
-      (after 'key-chord-mode
-        (key-chord-define evil-normal-state-map ";w" 'ace-jump-word-mode)
-        (key-chord-define evil-normal-state-map ";c" 'ace-jump-char-mode)
-        (key-chord-define evil-normal-state-map ";l" 'ace-jump-line-mode)
-        )
-      )
-    )
+                     '(?n ?v ?b ?m ?c ?x ?z))))))
   :config
   (progn
     (use-package ace-jump-buffer
@@ -81,9 +65,9 @@
        ace-jump-buffer-in-one-window
        ace-jump-buffer-other-window
        )
-      :ensure ace-jump-buffer)
-    )
-  )
+      :ensure ace-jump-buffer)))
+
+
 (use-package guide-key
   :ensure guide-key
   :diminish guide-key-mode
@@ -93,21 +77,20 @@
     (setq guide-key/recursive-key-sequence-flag t)
     (guide-key-mode 1)
     (setq guide-key/idle-delay 1.5)
-    (setq guide-key/popup-window-position 'top)
-    )
-  )
+    (setq guide-key/popup-window-position 'top)))
+
+
 (use-package expand-region
   :ensure expand-region
-  :init (progn
-          (after 'evil
-            (define-key evil-normal-state-map (kbd "C-e") 'er/expand-region)
-            (define-key evil-visual-state-map (kbd "C-e") 'er/expand-region)
-            )
-          )
-  :config (progn
-            (after 'expand-region-autoloads
-              (global-set-key (kbd "C-=") 'er/expand-region))
-            )
-  )
+  :init
+  (progn
+    (after 'evil
+      (define-key evil-normal-state-map (kbd "C-e") 'er/expand-region)
+      (define-key evil-visual-state-map (kbd "C-e") 'er/expand-region)))
+  :config
+  (progn
+    (after 'expand-region-autoloads
+      (global-set-key (kbd "C-=") 'er/expand-region))))
+
 
 (provide 'my-interaction)
