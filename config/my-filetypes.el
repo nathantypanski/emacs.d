0a0a2d32265cb881c64a1c2577804bfd8396376c
Nathan Typanski <ntypanski@gmail.com>
2014-11-15 11:55:40 -0500

    my-filetypes: cleanup comments

--- config/my-filetypes.el
+++ config/my-filetypes.el
@@ -1,3 +1,10 @@
-;; zsh shell script mode
+;; my-filetypes.el
+;;
+;; This is for filetype-specific mode settings that don't belong anywhere
+;; else. In the case of language-specific settings that already have sufficient
+;; outside customization, place those files in their appropriate
+;; config/my-languages/ language file.
+
 (add-to-list 'auto-mode-alist '("/mutt" . mail-mode))
+
 (provide 'my-filetypes)
