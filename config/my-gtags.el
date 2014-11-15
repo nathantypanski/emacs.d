85f697b5c0eaeaa82b28a42969952be05a75f77c
Nathan Typanski <ntypanski@gmail.com>
2014-11-15 12:15:16 -0500

    gtags: style

--- config/my-gtags.el
+++ config/my-gtags.el
@@ -16,9 +16,11 @@
             (buffer-substring (point-min) (1- (point-max)))
           nil)))
 
+
     (defun my-gtags-update ()
       "Make GTAGS incremental update"
       (call-process "global" nil nil nil "-u"))
+
 
     (defun my-gtags-update-single (filename)
       "Update Gtags database for changes in a single file"
@@ -26,6 +28,7 @@
       (start-process
        "update-gtags" "update-gtags" "bash" "-c"
        (concat "cd " (my-gtags-root-dir) " && gtags --single-update " filename)))
+
 
     (defun my-gtags-update-current-file ()
       (interactive)
@@ -37,20 +40,22 @@
       (my-gtags-update-single filename)
       (message "Gtags updated for %s" filename)))
 
+
     (defun my-gtags-update-hook ()
       "Update GTAGS file incrementally upon saving a file"
       (when ggtags-mode
         (when (my-gtags-root-dir)
           (my-gtags-update-current-file))))
 
+
     (add-hook 'after-save-hook #'my-gtags-update-hook)
+
 
     (defun my-setup-ggtags ()
       "Configure the modes I want to use ggtags."
         (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
               (ggtags-mode 1)))
-    (add-hook 'c-mode-common-hook 'my-setup-ggtags)
-   )
-  )
+
+    (add-hook 'c-mode-common-hook 'my-setup-ggtags)))
 
 (provide 'my-gtags)
