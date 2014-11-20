533b1e10a1518632dd74b5486abf91c8d7913210
Nathan Typanski <ntypanski@gmail.com>
2014-11-19 22:05:21 -0500

    rm commented line in my-haskell.el

--- config/languages/my-haskell.el
+++ config/languages/my-haskell.el
@@ -5,7 +5,6 @@
 (use-package haskell-mode
   :ensure haskell-mode
   :commands haskell-mode
-;;  :mode '"\\.hs\\'" . haskell-mode)
   :config
   (progn
     (setq haskell-process-show-debug-tips nil)
