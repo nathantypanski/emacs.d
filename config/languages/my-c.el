497f6a2503f44881209f9157161caa17c948f445
Nathan Typanski <ntypanski@gmail.com>
2014-11-19 22:04:50 -0500

    fix my-c.el to be less horrible

--- config/languages/my-c.el
+++ config/languages/my-c.el
@@ -1,115 +1,76 @@
 (require 'google-c-style)
-;;(c-add-style "Google" google-c-style t)
 
 (setq c-default-style '((java-mode . "java")
                         (awk-mode . "awk")
                         (c++-mode . "google")
-;;                        (c++-mode . "Google") ;; makes things bug
                         (other . "linux")))
+
+
 
 (defun my-set-evil-shift-width ()
   "Set Evil's shift width for C editing."
   (after 'evil
-    (setq evil-shift-width 8)
-    )
-  )
+    (setq evil-shift-width 8)))
 
 (add-hook 'c-initialization-hook 'my-set-evil-shift-width)
 
 (setq c-hungry-delete-key t)
 
-(defun my-c-mode-common-hook ()
-  (c-set-offset 'case-label '+)
-  )
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+(defun my-c-mode-common-setup ()
+  "Setup C/C++-mode common configurations."
+  (c-set-offset 'case-label '+))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-setup)
 
 (after 'evil
     (evil-define-key 'insert c-mode-map (kbd "TAB") 'c-indent-line-or-region)
     (evil-define-key 'normal c-mode-map (kbd "K")   'my-woman-entry)
     (evil-define-key 'insert c-mode-map (kbd "<backspace>") 'backward-delete-char-untabify)
     (evil-define-key 'normal c++-mode-map (kbd "K")   'my-woman-entry)
-    (evil-define-key 'insert c++-mode-map (kbd "<backspace>") 'backward-delete-char-untabify)
-)
+    (evil-define-key 'insert c++-mode-map (kbd "<backspace>") 'backward-delete-char-untabify))
 
 (use-package semantic
-  :disabled t
   :ensure semantic
-  ;; :commands
-  ;; (semantic-ia-fast-jump
-  ;;  semantic-complete-jump-local
-  ;;  semantic-complete-jump
-  ;;  semantic-complete-jump-local-members
-  ;;  semantic-symref-symbol
-  ;;  semantic-symref
-  ;;  semantic-complete-analyze-inline
-  ;;  senator-kill-tag
-  ;;  senator-copy-tag
-  ;;  senator-yank-tag
-  ;;  senator-copy-tag-to-register
-  ;;  semantic-force-refresh
-  ;;  senator-transpose-tags-up
-  ;;  senator-transpose-tags-down
-  ;;  semantic-analyze-possible-completions
-  ;;  semantic-mode)
+  :commands
+  (semantic-ia-fast-jump
+   semantic-complete-jump-local
+   semantic-complete-jump
+   semantic-complete-jump-local-members
+   semantic-symref-symbol
+   semantic-symref
+   semantic-complete-analyze-inline
+   senator-kill-tag
+   senator-copy-tag
+   senator-yank-tag
+   senator-copy-tag-to-register
+   semantic-force-refresh
+   senator-transpose-tags-up
+   senator-transpose-tags-down
+   semantic-analyze-possible-completions
+   semantic-mode)
   :init
   (progn
-    (global-semanticdb-minor-mode 1)
-    )
+    (global-semanticdb-minor-mode 1))
   :config
   (progn
     (setq semanticdb-default-save-directory
-        (expand-file-name "semanticdb" "~/.emacs.d/.semantic"))
-    )
-  )
+        (expand-file-name "semanticdb" "~/.emacs.d/.semantic"))))
 
 (use-package cedet
-  :disabled t
   :ensure cedet
-  :commands (
-             eassist-switch-h-cpp
-             )
+  :commands (eassist-switch-h-cpp)
   :init
   (progn
     (after 'evil
         (evil-define-key 'normal c++-mode-map (kbd "SPC o") 'eassist-switch-h-cpp)
         (evil-define-key 'normal c-mode-map   (kbd "SPC o") 'eassist-switch-h-c)
-    ))
+        (evil-set-initial-state 'eieio-custom-mode 'emacs)))
   :config
   (progn
     (require 'eassist)
     (semantic-mode)
     (semanticdb-enable-gnu-global-databases 'c-mode)
     ;; show semantic summary in minibuffer when I idle over a function
-    (global-semantic-idle-summary-mode)
-    )
-    )
-
-(use-package helm-gtags
-  :disabled t
-  :ensure helm-gtags
-  :commands (
-             helm-gtags-mode
-             helm-gtags-find-tag
-             helm-gtags-find-rtag
-             helm-gtags-find-symbol
-             helm-gtags-parse-file
-             helm-gtags-previous-history
-             helm-gtags-next-history
-             helm-gtags-pop-stack
-             )
-  :config
-  :init
-  (progn
-       ;; (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
-       ;; (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
-       ;; (define-key helm-gtags-mode-map (kbd "M-s")    'helm-gtags-find-symbol)
-       ;; (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
-       ;; (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-       ;; (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-       ;; (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-        (add-hook 'c-mode-hook 'helm-gtags-mode)
-        (add-hook 'c++-mode-hook 'helm-gtags-mode)
-        (add-hook 'asm-mode-hook 'helm-gtags-mode)
-    ))
+    (global-semantic-idle-summary-mode)))
 
 (provide 'my-c)
