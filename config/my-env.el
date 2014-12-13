4ce0a9e29fb42dd58c4ff4408aa009670d2870de
Nathan Typanski <ntypanski@gmail.com>
2014-12-13 16:57:42 -0500

    look before clobbering global environment variables
    Previously, I was setting environment variables like XDG_CONFIG_HOME by
    just aribtrarily binding them to "$HOME/.config" - which, indeed,
    involved a lookup in the global environment for the variable HOME.
    
    So why not check for XDG_CONFIG_HOME as well?
    
    This creates a new function, my-set-environment-or-default, which takes
    an environment variable name and an optional default argument. If the
    environment variable lookup fails and a default value is provided, then
    the environment variable is set from that default.

--- config/my-env.el
+++ config/my-env.el
@@ -2,9 +2,22 @@
 ;;
 ;; Environment variables settings.
 
+(defun my-set-environment-or-default (varname &optional default)
+  "Attempt to get VARNAME from the global environment and set it in Emacs.
 
-(setenv "XDG_CONFIG_HOME" (concat (getenv "HOME") "/.config"))
-(setenv "XDG_DATA_HOME" (concat (getenv "HOME") "/.local/share"))
-(setenv "GTAGSLIBPATH" (concat (getenv "XDG_DATA_HOME") "gtags"))
+If this fails and DEFAULT is provided, set VARNAME to the default value
+DEFAULT in the Emacs environment instead."
+  (if (getenv varname)
+      (setenv varname (getenv varname))
+      (if default (setenv varname default))))
+
+(my-set-environment-or-default "XDG_CONFIG_HOME"
+                               (concat (getenv "HOME") "/.config"))
+
+(my-set-environment-or-default "XDG_DATA_HOME"
+                               (concat (getenv "HOME") "/.local/share"))
+
+(my-set-environment-or-default "GTAGSLIBPATH"
+                               (concat (getenv "XDG_DATA_HOME") "gtags"))
 
 (provide 'my-env)
