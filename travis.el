3b948b0734a09bf0b65c06d08bb46c74612ed692
Nathan Typanski <ntypanski@gmail.com>
2014-12-13 23:25:46 -0500

    travis: user-emacs-directory should end with a "/"

--- travis.el
+++ travis.el
@@ -1,12 +1,6 @@
-(setq user-emacs-directory (getenv "TRAVIS_BUILD_DIR"))
+(setq user-emacs-directory (concat (getenv "TRAVIS_BUILD_DIR") "/"))
 (print (concat "Using emacs directory: " user-emacs-directory))
 (add-to-list 'load-path user-emacs-directory)
-
-(add-to-list 'load-path (concat user-emacs-directory "config"))
-(add-to-list 'load-path (concat user-emacs-directory "elisp"))
-(add-to-list 'load-path (concat user-emacs-directory "config" "/eyecandy"))
-(add-to-list 'load-path (concat user-emacs-directory "config" "/languages"))
-(add-to-list 'load-path (concat user-emacs-directory "/usr/share/emacs/site-lisp"))
 
 (print (concat "Load path: " user-emacs-directory))
 (require 'init)
