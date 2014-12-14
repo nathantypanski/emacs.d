31c1863671b60c2a8dacbc48e9246847293b895b
Nathan Typanski <ntypanski@gmail.com>
2014-12-13 23:19:26 -0500

    travis: fix the build
    This is a squash of my "make travis work" efforts:
    
    - try printing emacs directory
    - attempt load path debug
    
    The real fix:
    
    - user-emacs-directory should end with a "/"

--- travis.el
+++ travis.el
@@ -1,6 +1,7 @@
-(setq user-emacs-directory (getenv "TRAVIS_BUILD_DIR"))
-
+(setq user-emacs-directory (concat (getenv "TRAVIS_BUILD_DIR") "/"))
+(print (concat "Using emacs directory: " user-emacs-directory))
 (add-to-list 'load-path user-emacs-directory)
 
+(print (concat "Load path: " user-emacs-directory))
 (require 'init)
 (batch-byte-compile)
