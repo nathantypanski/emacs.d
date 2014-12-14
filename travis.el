10933d38313d7a724bbf81a2e7eef0109e51eb74
Nathan Typanski <ntypanski@gmail.com>
2014-12-13 23:21:49 -0500

    travis: attempt load path debug

--- travis.el
+++ travis.el
@@ -1,5 +1,13 @@
 (setq user-emacs-directory (getenv "TRAVIS_BUILD_DIR"))
 (print (concat "Using emacs directory: " user-emacs-directory))
 (add-to-list 'load-path user-emacs-directory)
+
+(add-to-list 'load-path (concat user-emacs-directory "config"))
+(add-to-list 'load-path (concat user-emacs-directory "elisp"))
+(add-to-list 'load-path (concat user-emacs-directory "config" "/eyecandy"))
+(add-to-list 'load-path (concat user-emacs-directory "config" "/languages"))
+(add-to-list 'load-path (concat user-emacs-directory "/usr/share/emacs/site-lisp"))
+
+(print (concat "Load path: " user-emacs-directory))
 (require 'init)
 (batch-byte-compile)
