81cb0fbf7f9a19da6f9d8426bf9a28ff9da62e1b
Nathan Typanski <ntypanski@gmail.com>
2015-03-29 14:36:01 -0400

    move saveplace, savehist, recentf to my-core.el

--- config/my-core.el
+++ config/my-core.el
@@ -194,4 +194,26 @@ This command only has an effect on graphical frames."
 
 (add-hook 'help-mode-hook 'my-setup-help-mode)
 
+(use-package saveplace
+  :config
+  (progn
+    (setq save-place-file (concat user-emacs-directory ".cache/places"))
+    (setq-default save-place t)))
+
+(use-package savehist
+  :config
+    (progn
+        (setq savehist-file (concat user-emacs-directory ".cache/savehist")
+            savehist-additional-variables '(search ring regexp-search-ring)
+            savehist-autosave-interval 60)
+        (savehist-mode t)))
+
+(use-package recentf
+  :config
+  (progn
+    (setq recentf-save-file (concat user-emacs-directory ".cache/recentf")
+          recentf-max-saved-items 1000
+          recentf-max-menu-items 500)
+    (recentf-mode +1)))
+
 (provide 'my-core)
--- config/my-dired.el
+++ config/my-dired.el
@@ -1,27 +1,5 @@
 (put 'dired-find-alternate-file 'disabled nil)
 
-(use-package saveplace
-  :config
-  (progn
-    (setq save-place-file (concat user-emacs-directory ".cache/places"))
-    (setq-default save-place t)))
-
-(use-package savehist
-  :config
-    (progn
-        (setq savehist-file (concat user-emacs-directory ".cache/savehist")
-            savehist-additional-variables '(search ring regexp-search-ring)
-            savehist-autosave-interval 60)
-        (savehist-mode t)))
-
-(use-package recentf
-  :config
-  (progn
-    (setq recentf-save-file (concat user-emacs-directory ".cache/recentf")
-          recentf-max-saved-items 1000
-          recentf-max-menu-items 500)
-    (recentf-mode +1)))
-
 (use-package dired-x
   :init
   (progn
