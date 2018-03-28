fcd8600a9e5a661add3ae923cbc2fe5a4dd54ddb
Nathan Typanski <nathan.typanski@mongodb.com>
2018-03-28 04:50:57 -0400

    org-mode: improvements

--- config/languages/my-org.el
+++ config/languages/my-org.el
@@ -1,4 +1,3 @@
-
 (use-package org
   :commands (org-mode org-capture org-agenda orgtbl-mode)
   :init
@@ -20,6 +19,23 @@
     (defface org-block-end-line
       '((t ( org-meta-line :background "gray27" :overline "gray20" :underline "gray20" :height 0.8)))
       "Face used for the line delimiting the end of source blocks.")
+
+    ;; better default
+    (add-hook 'org-mode-hook 'org-indent-mode)
+
+    (setq org-agenda-files (list "~/org/agenda.org"))
+
+    ;; https://emacs.stackexchange.com/questions/9528/is-it-possible-to-remove-emsp-from-clock-report-but-preserve-indentation
+    (defun my-org-clocktable-indent-string (level)
+      (if (= level 1)
+          ""
+        (let ((str "^"))
+          (while (> level 2)
+            (setq level (1- level)
+                  str (concat str "--")))
+          (concat str "-> "))))
+
+    (advice-add 'org-clocktable-indent-string :override #'my-org-clocktable-indent-string)
 
     (setq org-src-fontify-natively t)
     (use-package ob
