311f231790877daab4b8594c8a85a5c054823759
ndt <ndt@nathantypanski.com>
2022-03-08 09:29:08 -0500

    elisp: formatting

--- config/languages/my-elisp.el
+++ config/languages/my-elisp.el
@@ -14,17 +14,14 @@
     (defun my-lisp-hook ()
       (progn
         (elisp-slime-nav-mode)
-        (turn-on-eldoc-mode)
-        )
-      )
+        (turn-on-eldoc-mode)))
     (add-hook 'emacs-lisp-mode-hook 'my-lisp-hook)
     (add-hook 'lisp-interaction-mode-hook 'my-lisp-hook)
     (add-hook 'ielm-mode-hook 'my-lisp-hook)
     (defun my-jump-to-elisp-docs (sym-name)
       "Jump to a pane and do elisp-slime-nav-describe-elisp-thing-at-point"
       (interactive (list (elisp-slime-nav--read-symbol-at-point)))
-      (help-xref-interned (intern sym-name)))
-    )
+      (help-xref-interned (intern sym-name))))
   :config
   (progn
     (after 'evil
