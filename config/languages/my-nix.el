ebaf306a1ec1962312b3da52158113b2e2d91021
ndt <ndt@nathantypanski.com>
2022-11-07 10:03:36 -0500

    nix: comment out missing  packages

--- config/languages/my-nix.el
+++ config/languages/my-nix.el
@@ -11,12 +11,12 @@
   :ensure nix-mode
   :mode "\\.nix\\'")
 
-(use-package nix-repl
-  :ensure nix-mode
-  :commands (nix-repl))
+;; (use-package nix-repl
+;;   :ensure nix-mode
+;;   :commands (nix-repl))
 
-(use-package nix-shell
-  :ensure nix-mode
-  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+;; (use-package nix-shell
+;;   :ensure nix-mode
+;;   :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
 
 (provide 'my-nix)
