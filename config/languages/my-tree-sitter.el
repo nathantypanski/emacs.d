12dbe90bd436426a15bc65898c842c4418745be1
ndt <ndt@nathantypanski.com>
2024-01-29 12:00:28 -0500

    rm unnecessary progn

--- config/languages/my-tree-sitter.el
+++ config/languages/my-tree-sitter.el
@@ -1,13 +1,9 @@
 (use-package tree-sitter
   :commands tree-sitter
-  :ensure tree-sitter
-  :config
-  (progn))
+  :ensure tree-sitter)
 
 (use-package tree-sitter-langs
   :commands tree-sitter-langs
-  :ensure tree-sitter-langs
-  :config
-  (progn))
+  :ensure tree-sitter-langs)
 
 (provide 'my-tree-sitter)
