b3cb8b46e726a934e2ecd065919ef5dbe9234f32
ndt <mail@nathantypanski.com>
2025-05-08 04:32:11 -0400

    use better completion config

--- config/my-completion.el
+++ config/my-completion.el
@@ -5,10 +5,30 @@
 
 (use-package ivy
   :ensure ivy
+  :diminish
+  :bind (("C-s" . swiper)        ;; in-buffer search
+         ("C-c C-r" . ivy-resume) ;; resume last Ivy completion
+         ("M-x" . counsel-M-x)   ;; smarter M-x
+         ("C-x C-f" . counsel-find-file) ;; fuzzy find files
+         ("C-x b" . ivy-switch-buffer)) ;; switch buffers
   :config (progn
             (after 'projectile
               (progn
                 (setq projectile-completion-system 'ivy)))
-            (ivy-mode)))
+            (ivy-mode))
+  :custom
+  (ivy-use-virtual-buffers t)   ;; show recent files and bookmarks
+  (enable-recursive-minibuffers t)
+  (ivy-count-format "(%d/%d) ") ;; show candidate count
+  (ivy-wrap t))
+
+;; Counsel: Ivy-enhanced versions of built-ins
+(use-package counsel
+  :after ivy
+  :config (counsel-mode 1))
+
+;; Swiper: in-buffer search with Ivy interface
+(use-package swiper
+  :after ivy)
 
 (provide 'my-completion)
