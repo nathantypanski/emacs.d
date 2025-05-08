7065a621a95100e87e1664addc02628f739d579f
ndt <mail@nathantypanski.com>
2025-05-08 04:49:06 -0400

    tweak helm (for some reason)

--- config/my-helm.el
+++ config/my-helm.el
@@ -13,63 +13,56 @@
       "Display a helm buffer in ido. Send the purists screaming."
       (interactive)
       (ido-buffer-internal 'display 'display-buffer nil nil nil 'ignore)))
-    (setq helm-display-function 'helm-default-display-buffer)
-    (setq helm-adaptive-history-file "~/.emacs.d/helm-adapative-history")
+  (setq helm-display-function 'helm-default-display-buffer)
+  (setq helm-adaptive-history-file "~/.emacs.d/helm-adapative-history")
 
-    (define-key helm-map (kbd "C-p") 'helm-execute-persistent-action)
-    (define-key helm-map (kbd "C-n") 'helm-delete-minibuffer-contents)
-    (define-key helm-map (kbd "C-j") 'helm-next-line)
-    (define-key helm-map (kbd "C-k") 'helm-previous-line)
+  (define-key helm-map (kbd "C-p") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-n") 'helm-delete-minibuffer-contents)
+  (define-key helm-map (kbd "C-j") 'helm-next-line)
+  (define-key helm-map (kbd "C-k") 'helm-previous-line)
 
+  (require 'helm-files)
 
-    (require 'helm-files)
-
-
-    (after 'projectile
-      (use-package helm-projectile
-        :ensure helm-projectile))
+  (after 'projectile
+    (use-package helm-projectile
+      :ensure helm-projectile))
 
 
-    (defun helm-jump ()
-      "Find files with helm, but be smart about buffers and recent files."
-      (interactive)
-      (let ((helm-ff-transformer-show-only-basename nil))
-        (helm-other-buffer '(helm-projectile-sources-list
-                             helm-source-buffers-list
-                             helm-source-recentf
-                             helm-source-bookmarks
-                             helm-source-file-cache
-                             helm-source-files-in-current-dir
-                             helm-source-locate
-                             helm-source-buffer-not-found)
-                           "*helm jump*")))
+  (defun helm-jump ()
+    "Find files with helm, but be smart about buffers and recent files."
+    (interactive)
+    (let ((helm-ff-transformer-show-only-basename nil))
+      (helm-other-buffer '(helm-projectile-sources-list
+                           helm-source-buffers-list
+                           helm-source-recentf
+                           helm-source-bookmarks
+                           helm-source-file-cache
+                           helm-source-files-in-current-dir
+                           helm-source-locate
+                           helm-source-buffer-not-found)
+                         "*helm jump*")))
 
-    (setq helm-command-prefix-key "C-c h")
-    (setq helm-quick-update t)
-
-
-    (use-package helm-swoop
-      :ensure helm-swoop
-      :config
-      (progn
-        ;; Don't start searching for the thing at point by default.
-        ;; Let me type it.
-        (setq helm-swoop-pre-input-function (lambda () ()))
-        (after 'evil
-          (define-key evil-normal-state-map (kbd "SPC l")   'helm-swoop))))
+  (setq helm-command-prefix-key "C-c h")
+  (setq helm-quick-update t)
 
 
-    (after 'evil-leader
-        (evil-leader/set-key "b" 'helm-mini)
-        (evil-leader/set-key "i" 'helm-imenu))
+  (use-package helm-swoop
+    :ensure helm-swoop
+    :config
+    (progn
+      ;; Don't start searching for the thing at point by default.
+      ;; Let me type it.
+      (setq helm-swoop-pre-input-function (lambda () ()))
+      (after 'evil
+        (define-key evil-normal-state-map (kbd "SPC l")   'helm-swoop))))
 
-    (use-package helm-dash
+  (use-package helm-dash
     :ensure helm-dash
     :config
     (progn))
 
-    (after 'flycheck
-      (use-package helm-flycheck
-        :ensure helm-flycheck)))
+  (after 'flycheck
+    (use-package helm-flycheck
+      :ensure helm-flycheck)))
 
 (provide 'my-helm)
