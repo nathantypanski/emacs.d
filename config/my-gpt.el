4f5e446aa268c1138da78ac2ca52465950b8e5ed
ndt <mail@nathantypanski.com>
2025-05-11 14:54:20 -0400

    gpt: set default prompt

--- config/my-gpt.el
+++ config/my-gpt.el
@@ -1,3 +1,10 @@
+;; my-gpt.el
+;;
+;; configure llm interactions
+
+(defvar my-gptel-system-prompt
+"You are a LLM running inside Emacs. Your responses are inserted literally into the buffer where the prompt is sent - usually code in the language being discussed. Do not use markdown or org to structure your comments. Instead, structure in alignment with the surrounding text. Put your commentary in comments (e.g., `;;` for elisp, `//` for go, ...)."
+  "my preferred system prompt for gptel")
 
 (use-package gptel
   :straight (gptel :type git :host github :repo "karthink/gptel")
@@ -10,11 +17,13 @@
   :config
   ;; Set default mode for gptel conversation
   (setq gptel-default-mode 'org-mode)
+  ;; is this correct? I want to make sure my directive is default.
+  (add-to-list 'gptel-directives (cons 'Inline my-gptel-system-prompt)))
 
-  ;; Optionally, set any other configurations for better integration
-  ;; For example, you might want to customize gptel's output or other settings here.
-  ;; (setq gptel-output-buffer "*GPTel Output*")
-)
+  ;; inside gptel-menu, if a region is selected and i hit - to let me customize an option, it always errors with the following:
+  ;;
+  ;;  M-[ < M-[ < is undefined
+  ;;
+  ;; this makes no sense to me.
 
 (provide 'my-gpt)
-
