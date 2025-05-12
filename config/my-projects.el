afb483448eeceec0a402d4204470b286c773163f
ndt <mail@nathantypanski.com>
2025-05-12 18:01:56 -0400

    remove neotree

--- config/my-projects.el
+++ config/my-projects.el
@@ -38,25 +38,4 @@
             ".svn"
             "build"))))
 
-(use-package neotree
-  :ensure neotree
-  :config
-  (progn
-    (defun neo-buffer--insert-header ()
-      (let ((start (point)))
-        (set-text-properties start (point) '(face neo-header-face)))
-      (neo-buffer--newline-and-begin))
-    (after 'evil
-    (evil-set-initial-state 'neotree-mode 'normal)
-    (evil-define-key 'normal neotree-mode-map
-      (kbd "RET") 'neotree-enter
-      (kbd "c")   'neotree-create-node
-      (kbd "r")   'neotree-rename-node
-      (kbd "d")   'neotree-delete-node
-      (kbd "j")   'neotree-next-node
-      (kbd "k")   'neotree-previous-node
-      (kbd "SPC") 'neotree-change-root
-      (kbd "q")   'neotree-hide
-      (kbd "l")   'neotree-enter))))
-
 (provide 'my-projects)
