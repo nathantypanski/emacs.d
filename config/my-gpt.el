9c15454c4dac70535b72319ccd92a1545009071d
ndt <mail@nathantypanski.com>
2025-05-21 04:42:53 -0400

    gptel: tweaks

--- config/my-gpt.el
+++ config/my-gpt.el
@@ -1,4 +1,4 @@
-;; my-gpt.el
+;; my-gpt.el -*- lexical-binding: t; -*-
 ;;
 ;; configure llm interactions
 
@@ -11,14 +11,12 @@
   :defer t
   :commands (gptel gptel-menu gptel-send gptel-request)
   :hook (gptel-mode . visual-line-mode)
-  :init
-  (let ((api-key (getenv "OPENAI_API_KEY")))
-    (if api-key
-        (setq gptel-api-key api-key)))
   :config
+  (setq gptel-api-key (getenv "OPENAI_API_KEY"))
   ;; Set default mode for gptel conversation
   (setq gptel-default-mode 'org-mode)
   ;; is this correct? I want to make sure my directive is default.
   (add-to-list 'gptel-directives (cons 'Inline my-gptel-system-prompt)))
+
 
 (provide 'my-gpt)
