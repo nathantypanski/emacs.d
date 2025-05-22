83de4017411fae95ec693ccd68587dab632e6065
ndt <mail@nathantypanski.com>
2025-05-22 01:46:13 -0400

    my-ag: modernize

--- config/my-ag.el
+++ config/my-ag.el
@@ -1,27 +1,24 @@
 ;; -*- lexical-binding: t; -*-
+
 (use-package ag
   :ensure ag
   :commands (ag ag-mode ag-files ag-regexp)
+  :hook ((age-mode-hook . my-setup-ag))
+  :custom
+  (ag-highlight-search t)
+  (ag-reuse-buffers t)
   :init
-  (progn
-    (setq ag-highlight-search t)
-    (setq ag-reuse-buffers t)
     (defun my-setup-ag ()
       "Function called to set my ag stuff up."
       (toggle-truncate-lines t)
-      (switch-to-buffer-other-window "*ag search*")
-      )
+      (switch-to-buffer-other-window "*ag search*"))
     (add-hook 'ag-mode-hook 'my-setup-ag)
     (after 'evil
-      (evil-set-initial-state 'ag-mode 'normal)
-      ;; unbind kill window
-      )
-    )
+      (evil-set-initial-state 'ag-mode 'normal))
   :config
-  (progn
       (define-key ag-mode-map (kbd "k") 'nil)
       (evil-define-key 'normal ag-mode-map (kbd "k") 'nil)
       (evil-define-key 'motion ag-mode-map (kbd "k") 'compilation-previous-error)
-      (evil-define-key 'motion ag-mode-map (kbd "j") 'compilation-next-error)))
+      (evil-define-key 'motion ag-mode-map (kbd "j") 'compilation-next-error))
 
 (provide 'my-ag)
