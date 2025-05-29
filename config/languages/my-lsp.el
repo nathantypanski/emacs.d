66f2fe99fc5e5848b68803771590bb81f245a0c8
ndt <mail@nathantypanski.com>
2025-05-28 20:23:24 -0400

    my-lsp: use eldoc-box; add elgot-ws-config

--- config/languages/my-lsp.el
+++ config/languages/my-lsp.el
@@ -11,6 +11,13 @@
   (eglot-autoshutdown t)
   (eglot-code-action-indicator "*")
   :config
+  (setq-default eglot-workspace-configuration
+                '(:rust-analyzer
+                  (:cargo
+                   (:buildScripts (:enable t))
+                   :procMacro (:enable t)
+                   :diagnostics (:disabled ["unresolved-proc-macro" "unresolved-macro-call"]))))
+
   ;; Explicitly set pylsp as the server (if necessary)
   (add-to-list 'eglot-server-programs
                '(python-mode . ("pylsp"))))
@@ -20,6 +27,13 @@
   :custom
   (eldoc-echo-area-use-multiline-p t)
   (eldoc-echo-area-max-lines 8)
+  :config)
+
+(use-package eldoc-box
+  :after (eldoc eglot)
+  :demand t
+  :ensure t
+  :straight t
   :config
   (general-define-key
    :states 'normal
