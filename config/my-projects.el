4806da8ea3509f470585cbb6d43038285dd42f53
ndt <mail@nathantypanski.com>
2025-06-17 02:44:58 -0400

    projectile: don't use progn

--- config/my-projects.el
+++ config/my-projects.el
@@ -13,29 +13,28 @@
   :ensure projectile
   :diminish projectile-mode
   :config
-  (progn
-    (setq projectile-cache-file (concat user-emacs-directory ".cache/projectile.cache"))
-    (setq projectile-known-projects-file (concat user-emacs-directory "projectile-bookmarks.eld"))
-    (add-to-list 'projectile-globally-ignored-directories "elpa")
-    (add-to-list 'projectile-globally-ignored-directories ".cache")
-    (add-to-list 'projectile-globally-ignored-directories "node_modules")
-    (projectile-global-mode 1)
-    ;; automatically dired in projectile-switch-project
-    (setq projectile-switch-project-action 'projectile-dired)
-    (setq projectile-completion-system 'default)
-    (setq projectile-enable-caching t)
-    ;; use ripgrep/fd if available
-    (setq projectile-indexing-method 'alien)
-    (setq projectile-globally-ignored-directories
-          '(".idea"
-            ".eunit"
-            ".git"
-            ".hg"
-            ".fslckout"
-            ".bzr"
-            "_darcs"
-            ".tox"
-            ".svn"
-            "build"))))
+  (setq projectile-cache-file (concat user-emacs-directory ".cache/projectile.cache"))
+  (setq projectile-known-projects-file (concat user-emacs-directory "projectile-bookmarks.eld"))
+  (add-to-list 'projectile-globally-ignored-directories "elpa")
+  (add-to-list 'projectile-globally-ignored-directories ".cache")
+  (add-to-list 'projectile-globally-ignored-directories "node_modules")
+  (projectile-global-mode 1)
+  ;; automatically dired in projectile-switch-project
+  (setq projectile-switch-project-action 'projectile-dired)
+  (setq projectile-completion-system 'default)
+  (setq projectile-enable-caching t)
+  ;; use ripgrep/fd if available
+  (setq projectile-indexing-method 'alien)
+  (setq projectile-globally-ignored-directories
+        '(".idea"
+          ".eunit"
+          ".git"
+          ".hg"
+          ".fslckout"
+          ".bzr"
+          "_darcs"
+          ".tox"
+          ".svn"
+          "build")))
 
 (provide 'my-projects)
