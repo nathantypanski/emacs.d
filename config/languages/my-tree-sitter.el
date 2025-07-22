b54e9aa154bb0ac995a9ae77fdf0ac49dee97b0b
ndt <mail@nathantypanski.com>
2025-07-22 16:35:34 -0400

    treesit: install missing grammar

--- config/languages/my-tree-sitter.el
+++ config/languages/my-tree-sitter.el
@@ -27,29 +27,29 @@
         ;;   (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
         ;;   (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
-  ;; Auto-install missing grammars
-  (defun my/treesit-install-all-languages ()
-    "Install all configured tree-sitter languages."
-    (interactive)
-    (dolist (lang-config treesit-language-source-alist)
-      (let ((lang (car lang-config)))
-        (unless (treesit-language-available-p lang)
-          (message "Installing tree-sitter grammar for %s" lang)
-          (treesit-install-language-grammar lang)))))
+        ;; Auto-install missing grammars
+        (defun my/treesit-install-all-languages ()
+          "Install all configured tree-sitter languages."
+          (interactive)
+          (dolist (lang-config treesit-language-source-alist)
+            (let ((lang (car lang-config)))
+              (unless (treesit-language-available-p lang)
+                (message "Installing tree-sitter grammar for %s" lang)
+                (treesit-install-language-grammar lang)))))
 
-  ;; Set up auto-mode-alist for tree-sitter modes
-  (setq major-mode-remap-alist
-        '((bash-mode . bash-ts-mode)
-          (sh-mode . bash-ts-mode)
-          (c-mode . c-ts-mode)
-          (c++-mode . c++-ts-mode)
-          (python-mode . python-ts-mode)
-          (rust-mode . rust-ts-mode)
-          (go-mode . go-ts-mode)
-          (js-mode . js-ts-mode)
-          (typescript-mode . typescript-ts-mode)
-          (yaml-mode . yaml-ts-mode)))
+        ;; Set up auto-mode-alist for tree-sitter modes
+        (setq major-mode-remap-alist
+              '((bash-mode . bash-ts-mode)
+                (sh-mode . bash-ts-mode)
+                (c-mode . c-ts-mode)
+                (c++-mode . c++-ts-mode)
+                (python-mode . python-ts-mode)
+                (rust-mode . rust-ts-mode)
+                (go-mode . go-ts-mode)
+                (js-mode . js-ts-mode)
+                (typescript-mode . typescript-ts-mode)
+                (yaml-mode . yaml-ts-mode)))
 
-  (add-to-list 'major-mode-remap-alist '(sh-mode . bash-ts-mode)))
+        (add-to-list 'major-mode-remap-alist '(sh-mode . bash-ts-mode)))
 
 (provide 'my-tree-sitter)
