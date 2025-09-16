7c1f9d31eb1752fd88a87fe31a58da81559b4828
ndt <mail@nathantypanski.com>
2025-09-16 17:38:22 -0400

    add message show/hide keys

--- config/my-keys.el
+++ config/my-keys.el
@@ -9,8 +9,32 @@
   (progn
     (key-chord-mode 1)))
 
+;; Keep Messages buffer visible
+(defun my-show-messages-buffer ()
+  "Display *Messages* buffer in a side window."
+  (interactive)
+  (display-buffer-in-side-window
+   (get-buffer-create "*Messages*")
+   '((side . bottom) (window-height . 0.25))))
+
+(defun my-show-messages-transient ()
+  "Show *Messages* buffer temporarily."
+  (interactive)
+  (let ((win (display-buffer-in-side-window
+              (get-buffer "*Messages*")
+              '((side . bottom) (window-height . 0.4))))
+        (map (make-sparse-keymap)))
+    ;; Any key closes it
+    (define-key map [t] (lambda () (interactive) (delete-window win)))
+    (set-transient-map map t)))
+
 (use-package general
   :ensure t
-  :demand t)
+  :demand t
+  :config
+  (general-define-key
+   :override nil
+   "C-c m" 'my-show-messages-transient)
+  )
 
 (provide 'my-keys)
